89815a25af3059eb6650a69c0c62b67e1b9d3d6b
Levi-F06 <flanrees14@gmail.com>
2024-09-03 16:30:34 +0100

    :sparkles: Allow users to enter options
    Users choice will also be validated in the validateChoice function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function getComputerChoice() {
-  let options = ["rock", "paper", "scissors"];
+  const options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+  let choice = prompt("rock, paper, or scissors?").toLowerCase();
+  while (!validateChoice(choice)) {
+    choice = prompt("Invalid choice! enter 'rock' 'paper' or 'scissors'");
+  }
+  return choice;
+}
+
+function validateChoice(choice) {
+  const options = ["rock", "paper", "scissors"];
+  for (let i = 0; i < options.length; i++) {
+    if (choice === options[i]) {
+      return true;
+    }
+  }
+  return false;
 }
