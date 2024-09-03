f5b476cf19b799a00d8e32376a771d42a3fb650c
Levi-F06 <flanrees14@gmail.com>
2024-09-03 16:48:26 +0100

    :sparkles: Add full game logic
    This includes player vs computer scores, logic to decide who wins, and
    outputting the result and current score. Currently no way to replay
    other than refreshing the browser.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * 3)];
@@ -20,3 +23,37 @@ function validateChoice(choice) {
   }
   return false;
 }
+
+function playRound(humanChoice, computerChoice) {
+  let result;
+  switch (humanChoice) {
+    case "rock":
+      result = computerChoice === "scissors" ? "win" : "loss";
+      break;
+    case "paper":
+      result = computerChoice === "rock" ? "win" : "loss";
+      break;
+    case "scissors":
+      result = computerChoice === "paper" ? "win" : "loss";
+      break;
+  }
+  return result;
+}
+
+function playGame() {
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+
+  if (playRound(humanChoice, computerChoice) === "win") {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+  console.log(`Player score : ${humanScore}`);
+  console.log(`Computer score : ${computerScore}`);
+  console.log("Press the button to play again!");
+}
+
+playGame();
